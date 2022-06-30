63960f1e9ea9cabd83acfb66f7db001bd064c56d
tom yuan <t1123425@gmail.com>
2022-07-01 00:21:56 +0800

    add abi json & contract address & mint nft function

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,90 @@
-import React,{useEffect}from "react";
+import React,{useEffect,useState}from "react";
+import { ethers } from "ethers";
+import myEpicNft from "./utils/MyEpicNFT.json";
 import './styles/App.css';
 // const twitterLogo = require('./assets/twitter-logo.svg');
 import twitterLogo from "./assets/twitter-logo.svg";
-
+import { ExternalProvider } from "@ethersproject/providers";
+declare global { interface Window { ethereum?: ExternalProvider; } }
 // Constants
-const TWITTER_HANDLE:String = '_buildspace';
+const TWITTER_HANDLE:string = '_buildspace';
 const TWITTER_LINK:string = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK:String = '';
+const OPENSEA_LINK:string = '';
 const TOTAL_MINT_COUNT:Number = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletConnected = async () => {
+     const { ethereum } =  window;
+     if(!ethereum){
+      console.log("Make sure you have metamask!");
+      return;
+     }else{
+      console.log("We have the ethereum object", ethereum);
+     }
+
+     const accounts = await ethereum.request?.({method: 'eth_accounts'});
+    //  console.log('accounts',accounts);
+     if (accounts.length !== 0) {
+      const account:string = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+  const connectWallet = async () =>  {
+    try{
+     const { ethereum } =  window;
+     if(!ethereum){
+      alert("Get MetaMask!");
+      return;
+     }
+     /*
+      * Fancy method to request access to account.
+      */
+     const accounts = await ethereum.request?.({ method: "eth_requestAccounts" });
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    }catch(error){
+      console.log(error);
+    }
+  }
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
+  //mintNFT
+  const askContractToMintNft = async () => {
+     const CONTRACT_ADDRESS:string = process.env.REACT_APP_NFT_CONTRACT?process.env.REACT_APP_NFT_CONTRACT:'';
+     try{
+          const { ethereum } = window;
+          if(ethereum){
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+            console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.makeAnEpicNFT();
+
+          console.log("Mining...please wait.")
+          await nftTxn.wait();
+          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+     }catch(error){
+       console.log(error)
+     }
+  }
   useEffect(()=>{
-    console.log(twitterLogo);
+    checkIfWalletConnected();
   },[])
   return (
     <div className="App">
@@ -27,7 +94,13 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === ''?(
+            renderNotConnectedContainer()
+          ):(
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
